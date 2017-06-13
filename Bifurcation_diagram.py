c6413d8f68695d202bf648c94241a9a479fa7ae2
bkhk06 <bkhk06@users.noreply.github.com>
2017-06-13 09:06:42 +0800

    Update Bifurcation_diagram.py

--- Bifurcation_diagram.py
+++ Bifurcation_diagram.py
@@ -6,16 +6,16 @@ xa=0.252
 xb=1.99
 
 C=n.linspace(xa,xb,100)
-print C
+print(C)
 iter=1000
 Y = n.ones(len(C))
+print(Y)
 
-for x in xrange(iter):
+for x in range(iter):
     Y = Y**2 - C   #get rid of early transients
 
-for x in xrange(iter): 
+for x in range(iter):
     Y = Y**2 - C
     p.plot(C,Y, '.', color = 'k', markersize = 2)
-
-
 p.show()
+
